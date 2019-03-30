b673c4f155fffe6aa08f6f9444f195d6c65cd799
gaoshuhuan <shuhuan.gao@mytogo.com>
2019-03-30 23:24:57 +0800

    提交cpp文件

---
+++ main.cpp
@@ -0,0 +1,110 @@
+//
+//  main.cpp
+//  AlgorithmDemo
+//
+//  Created by shuhuan on 2019/3/30.
+//  Copyright © 2019年 gsh. All rights reserved.
+//
+
+#include <iostream>
+#include "SelectSort.cpp"
+#include "SortTestHelper.h"
+
+using namespace std;
+
+
+template <typename T> //模版函数(泛型)
+/*
+ 选择排序
+ **/
+void selectionSort ( T arr[] , int n ){
+    
+    //  10, 39, 8, 7, 6, 5, 4, 3, 2, 1
+    //先取出最小的值 然后交换
+    
+    for ( int i  = 0 ;  i < n ; i ++ ){
+        
+        int minIndex = i;
+        
+        for ( int j = i + 1 ; j < n ; j ++ ){
+            
+            if( arr[j] < arr[minIndex]){
+                minIndex = j;
+            }
+        }
+        
+        swap( arr[i] , arr[minIndex] );  //交换函数
+    }
+}
+
+/*
+ 插入排序
+ **/
+template <typename T> //模版函数(泛型)
+void insertSort (T arr[] , int n ){
+    
+    //  10, 39, 8, 7, 6, 5, 4, 3, 2, 1
+    //取出第二个值，和第一个值交换，
+    //取出第三个值，和第二个值交换，和第一个值交换
+    
+    for ( int i = 1 ; i < n ; i ++ ){
+        
+        for( int j = i ; j > 0  ; j -- ){
+            if( arr[j] < arr[j-1]){
+                swap( arr[j] , arr[j-1] );
+            }else{
+                break;
+            }
+            
+        }
+        
+    }
+}
+
+/*
+ 插入排序优化
+ **/
+template <typename T> //模版函数(泛型)
+void insertOptimizeSort (T arr[] , int n ){
+    
+    //  10, 39, 8, 7, 6, 5, 4, 3, 2, 1
+    //取出第二个值，拷贝一份，然后和第一个值比较，如果大把第一个值移
+    //取出第三个值，拷贝一份，然后和第二个值比较，如果大把第二个值移，然后和第一个值比较
+
+    
+    for ( int i = 1 ; i < n ; i ++ ){
+        
+        T e = arr[i];
+        int j;
+        for(j = i ; j > 0  ; j -- ){
+            
+            if( arr[j-1] >e){
+                
+                arr[j] = arr[j-1];
+            }else{
+                break;
+            }
+            
+        }
+        arr[j] = e;
+    }
+}
+
+
+
+int main(int argc, const char * argv[]) {
+   
+    int arr[10] =  {10,39, 8, 7, 6, 5, 4, 3, 2, 1};
+
+    insertSort(arr, 10);
+    
+    for( int i = 0 ; i < 10 ; i ++ ){
+        cout<< arr[i] <<endl;
+    }
+    
+    int n  =  10;
+    SortTestHelper::testSor("Selcetion sotr", selectionSort , arr, n);
+    
+    
+    return 0;
+}
